a2935ee9101625a400174c535a700bfdfb209ee2
MarkoNadj <marko.nad@fer.hr>
2015-11-15 20:02:30 +0100

    dodana nova scena kao skica mogućeg rješenja za dohvat točaka putanje kod proceduralnog generiranja mape

---
+++ ai test/Assets/Scripts/MapPartController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPartController : MonoBehaviour {
+    private MapPartController northPart = null;//north adjacent map part
+    private MapPartController eastPart = null;//east adjacent map part
+    private MapPartController southPart = null;//south adjacent map part
+    private MapPartController westPart = null;//west adjacent map part
+
+    void Start () {
+
+	}
+
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Sets the north adjacent map part. When it is determined what map part is to the north 
+    /// of this one, linkToNorth() method is invoked, and waypoints that are on the north 
+    /// side of this map part get the reference to the waypoints that are on the south side 
+    /// of the north adjacent part.
+    /// </summary>
+    /// <param name="northPart">Reference to the script that controlls the map part to the north 
+    /// of this one.</param>
+    public void setNorthPart(MapPartController northPart) {
+        this.northPart = northPart;
+        linkToNorth();
+    }
+    public MapPartController getNorthPart() {
+        return northPart;
+    }
+
+    public void setEastPart(MapPartController eastPart) {
+        this.eastPart = eastPart;
+    }
+    public MapPartController getEastPart() {
+        return eastPart;
+    }
+
+    public void setSouthPart(MapPartController southPart) {
+        this.southPart = southPart;
+    }
+    public MapPartController getSouthPart() {
+        return southPart;
+    }
+
+    public void setWestPart(MapPartController westPart) {
+        this.westPart = westPart;
+    }
+    public MapPartController getWestPart() {
+        return westPart;
+    }
+
+    /// <summary>
+    /// This method is invoked once the north part of the map is known. Waypoints 
+    /// that are on the north side of this map part get the reference to the waypoints 
+    /// that are on the south side of the north adjacent part.
+    /// </summary>
+    private void linkToNorth() {
+        //not yet implemented
+    }
+
+}
--- ai test/Assets/Scripts/PointDescriptor.cs
+++ ai test/Assets/Scripts/PointDescriptor.cs
@@ -8,6 +8,12 @@ using System.Collections;
 public class PointDescriptor : MonoBehaviour {
     private int label = 0;//Pedestrians are allowed to move from current waypoint only to waypoints with the same label.
 
+    public Transform[] surroundingWaypoints;//array of waypoints that are accessible from this waypoint
+    public bool isNorthLink;//if this waypoint is on the north side of the map part, this variable will be set to true; waypoints that are a north link have references to all the waypoints on the south side of the north adjacent map part
+    public bool isEastLink;
+    public bool isSouthLink;
+    public bool isWestLink;
+
     /// <summary>
     /// Returns this waypoint's label.
     /// </summary>
@@ -23,5 +29,15 @@ public class PointDescriptor : MonoBehaviour {
     public void setLabel(int newLabel) {
         label = newLabel;
     }
+ /*  
+    /// <summary>
+    /// Method which will be used by pedestrians to determine their next target waypoint.
+    /// One waypoint from the array of surrounding waypoints is chosen and returned to 
+    /// the pedestrian who called this method.
+    /// </summary>
+    /// <returns></returns>
+    public Transform getAdjacentWaypoint() {
 
+    }
+*/
 }
