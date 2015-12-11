cb5f16bc8b92fb65b7a80be3093bfde9c850a670
Dominik Ivošević <dominik.dito@gmail.com>
2015-12-11 16:08:19 +0100

    Some code

---
+++ Assets/Scripts/SpeedLimitCollider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+  public class SpeedLimitCollider : MonoBehaviour
+  {
+    void OnTriggerEnter (Collider other)
+    {
+      Debug.Log("Enter");
+      Debug.Log(other.tag);
+      Debug.Log(other.name);
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+      Debug.Log("Exit");
+    }
+  }
+}
--- Assets/Scripts/WorldSpawnerDemo.cs
+++ Assets/Scripts/WorldSpawnerDemo.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class WorldSpawnerDemo : MonoBehaviour {
 
-	public GameObject straightRoad;
+  public GameObject straightRoad;
+  public GameObject curvedRoad;
+  
+  public GameObject car;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 3; ++i) {
-			Instantiate (straightRoad, new Vector3 (0, 0, i * 40), new Quaternion (0, 0, 0, 0));
-		}
+    // Add car
+    Instantiate(car, Vector3.zero, Quaternion.identity);
+    
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        Instantiate(straightRoad, new Vector3(j * 40, 0, i * 40), new Quaternion);
+      }
+    }
 	}
 }
