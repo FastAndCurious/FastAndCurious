910ae3c235de0fb3cd36ca03a1c0c05ff68fb53e
Stella Coumbassa <stella.coumbassa@fer.hr>
2015-12-11 12:36:21 +0100

    skripta za sudar s kockom

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col)
+    {
+       if(col.collider.tag == "Cube")
+        {
+			col.collider.attachedRigidbody.AddForce(1,1,1000, ForceMode.Acceleration);
+            
+        }
+    }
+	
+}
