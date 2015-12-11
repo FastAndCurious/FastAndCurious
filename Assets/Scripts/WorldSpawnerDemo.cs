2b947c7033e90c2ba49561fc0c1e752a7c91a0f3
Mladen Gelemanović <mladen.gelemanovic@fer.hr>
2015-12-11 13:03:11 +0100

    Demo instanciranja blokova ceste

---
+++ Assets/Scripts/WorldSpawnerDemo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSpawnerDemo : MonoBehaviour {
+
+	public GameObject straightRoad;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 3; ++i) {
+			Instantiate (straightRoad, new Vector3 (0, 0, i * 40), new Quaternion (0, 0, 0, 0));
+		}
+	}
+}
