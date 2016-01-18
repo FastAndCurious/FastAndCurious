fe2949a23a8e012b028a5c3a7fe45e3e6c5b643e
Josip Tomić <josip.tomic@fer.hr>
2016-01-18 23:42:43 +0100

    dodan funkcionalan i gadan main menu bez pozadine

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public GameObject MainMenuHolder;
+    public GameObject CreditsHolder;
+
+    public void Play()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Credits()
+    {
+        MainMenuHolder.SetActive(false);
+        CreditsHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        CreditsHolder.SetActive(false);
+        MainMenuHolder.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
