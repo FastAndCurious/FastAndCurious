95fe7535085e53cc1af0b16ba5a9a5c05d0bcf8c
Josip Tomić <josip.tomic@fer.hr>
2016-01-19 18:55:31 +0100

    naštimavnje osvjetljenja u drugoj sceni

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class MenuManager : MonoBehaviour
 
     public void Play()
     {
-        Application.LoadLevel(1);
+        SceneManager.LoadScene("Main scene");
     }
 
     public void Credits()
